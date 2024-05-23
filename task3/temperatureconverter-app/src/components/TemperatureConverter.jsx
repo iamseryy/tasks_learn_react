9e1f6e0b0eee005e0ca5f2a525a9c69e79e33c84
desktop <psomail@yandex.ru>
2024-05-23 22:38:36 +0300

    task3.1 ready

--- task3/temperatureconverter-app/src/components/TemperatureConverter.jsx
+++ task3/temperatureconverter-app/src/components/TemperatureConverter.jsx
@@ -1,13 +1,44 @@
-import {Button, TextField} from "@mui/material";
+import {TextField} from "@mui/material";
+import {useState} from "react";
+import styles from './temperature_converter.module.css'
 
 
 export const TemperatureConverter = () => {
+    const [celsius, setCelsius] = useState('');
+    const [fahrenheit, setFahrenheit] = useState('');
+
+    const onChangeCelsius = (e) => {
+        setCelsius(e.target.value)
+        setFahrenheit('')
+
+        if(!isNaN(e.target.value) && e.target.value) setFahrenheit((e.target.value * 9/5) + 32)
+    }
+
+    const onChangeFahrenheit = (e) => {
+        setFahrenheit(e.target.value)
+        setCelsius('')
+
+        if(!isNaN(e.target.value) && e.target.value) setCelsius((e.target.value - 32) * 5/9)
+    }
+
+
     return (
         <div>
             <h2>Температура</h2>
-            <TextField id="standard-basic" label="Градус Цельсия (°C)" variant="standard" />
-            <TextField id="standard-basic" label="Градус Фаренгейта (°F)" variant="standard" />
-            <Button variant="outlined">Outlined</Button>
+            <div  className={styles.converter}>
+                <TextField
+                    label="Градус Цельсия (°C)"
+                    variant="standard"
+                    onChange={onChangeCelsius}
+                    value={celsius}
+                />
+                <TextField
+                    label="Градус Фаренгейта (°F)"
+                    variant="standard"
+                    onChange={onChangeFahrenheit}
+                    value={fahrenheit}
+                />
+            </div>
         </div>
     )
 }
